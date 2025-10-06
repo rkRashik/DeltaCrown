390f11633b65f19c59f680184d7af84d1ac57555
rashik <rkrashik@gmail.com>
2025-10-06 14:58:45 +0600

    Cleanup the project.

--- scripts/fix_test_fields.py
+++
@@ -1,9 +0,0 @@
-with open('tests/test_tournament_capacity.py', 'r', encoding='utf-8') as f:
-    content = f.read()
-
-content = content.replace('title="', 'name="')
-
-with open('tests/test_tournament_capacity.py', 'w', encoding='utf-8') as f:
-    f.write(content)
-
-print("Fixed all 'title=' to 'name=' in test file")
