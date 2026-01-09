39cfeb058475991b3927e8fe1cdb016fa3b62402
Md. Redwan Ul Islam Rashik <rkrashik@gmail.com>
2026-01-09 12:01:17 +0600

    fix user profile page phase 2

--- fix_migration.sql
+++
@@ -1,7 +0,0 @@
--- Drop incomplete form builder tables
-DROP TABLE IF EXISTS tournaments_form_response CASCADE;
-DROP TABLE IF EXISTS tournaments_registration_form CASCADE;
-DROP TABLE IF EXISTS tournaments_registration_form_template CASCADE;
-
--- Delete migration record so it can be re-run
-DELETE FROM django_migrations WHERE app = 'tournaments' AND name = '0010_create_form_builder_models';
